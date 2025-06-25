7cca5824480e251249b4bc4a111c48650f8df258
ganesh0737-aa <ganeshmah770@gmail.com>
2025-06-25 15:09:35 +0530

    hello

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,redirect
+from .models import Task
+
+# Create your views here.
+def index(request):
+    tasks=Task.objects.all()
+    return render(request,'index.html',{'tasks':tasks})
+
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+      task.completed = not task.completed
+      task.save()
+      return redirect('index')
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('index')
